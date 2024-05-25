3e5377df67827e25a6eecc126ed35a18bd7fa15c
denys <work.deniell90@gmail.com>
2024-05-25 17:28:53 +0300

    Add homework 21

--- tests/registrationForm.spec.ts
+++ tests/registrationForm.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { RegistrationForm } from '../page-objects/forms/registrationFormPOM';
+import { RegistrationForm } from '../page-objects/forms/registrationForm';
 import { correctEmail, incorrectEmail, correctPassword, incorrectPassword, numbersData, oneSymbolData } from '../test-data/registrationData';
 
 let registrationForm: RegistrationForm;
@@ -26,7 +26,7 @@ test.describe('Testing Registration form', () => {
       await expect(page).toHaveURL('/');
     })
 
-    test('Registration with correct data', async ({ page }) => {
+    test('Registration with correct data', async () => {
       await registrationForm.nameField.fill('TestUserOne');
       await registrationForm.lastNameField.fill('LastName');
       await registrationForm.emailField.fill(correctEmail);
@@ -39,25 +39,25 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Name" field', () => {
 
-    test('Leave name field blank', async ({ page }) => {
+    test('Leave name field blank', async () => {
       await registrationForm.nameField.focus();
       await registrationForm.nameField.blur();
       await expect(registrationForm.errorNote).toHaveText('Name required');
     });
 
-    test('Enter incorrect name', async ({ page }) => {
+    test('Enter incorrect name', async () => {
       await registrationForm.nameField.pressSequentially(numbersData);
       await registrationForm.lastNameField.focus();
       await expect(registrationForm.errorNote).toHaveText('Name is invalid');
     });
 
-    test('Enter incorrect number of characters for the name', async ({ page }) => {
+    test('Enter incorrect number of characters for the name', async () => {
       await registrationForm.nameField.pressSequentially(oneSymbolData);
       await registrationForm.lastNameField.focus();
       await expect(registrationForm.errorNote).toHaveText('Name has to be from 2 to 20 characters long');
     });
 
-    test('Check border color of the name field', async ({ page }) => {
+    test('Check border color of the name field', async () => {
       await registrationForm.nameField.focus();
       await registrationForm.nameField.blur();
       await expect(registrationForm.nameField).toHaveCSS('color','rgb(73, 80, 87)');
@@ -66,25 +66,25 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Last name" field', () => {
 
-    test('Leave last name field blank', async ({ page }) => {
+    test('Leave last name field blank', async () => {
       await registrationForm.lastNameField.focus();
       await registrationForm.lastNameField.blur();
       await expect(registrationForm.errorNote).toHaveText('Last name required');
     });
 
-    test('Enter incorrect last name', async ({ page }) => {
+    test('Enter incorrect last name', async () => {
       await registrationForm.lastNameField.pressSequentially(numbersData);
       await registrationForm.emailField.focus();
       await expect(registrationForm.errorNote).toHaveText('Last name is invalid');
     });
 
-    test('Enter incorrect number of characters for the last name', async ({ page }) => {
+    test('Enter incorrect number of characters for the last name', async () => {
       await registrationForm.lastNameField.pressSequentially(oneSymbolData);
       await registrationForm.emailField.focus();
       await expect(registrationForm.errorNote).toHaveText('Last name has to be from 2 to 20 characters long');
     });
 
-    test('Check border color of the last name field', async ({ page }) => {
+    test('Check border color of the last name field', async () => {
       await registrationForm.lastNameField.focus();
       await registrationForm.lastNameField.blur();
       await expect(registrationForm.lastNameField).toHaveCSS('color','rgb(73, 80, 87)');
@@ -93,19 +93,19 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Email" field', () => {
 
-    test('Enter incorrect email', async ({ page }) => {
+    test('Enter incorrect email', async () => {
       await registrationForm.emailField.pressSequentially(incorrectEmail);
       await registrationForm.passwordField.focus();
       await expect(registrationForm.errorNote).toHaveText('Email is incorrect');     
     });
 
-    test('Leave email field blank', async ({ page }) => {
+    test('Leave email field blank', async () => {
       await registrationForm.emailField.focus();
       await registrationForm.emailField.blur();
       await expect(registrationForm.errorNote).toHaveText('Email required');
     });
 
-    test('Check border color of the email field', async ({ page }) => {
+    test('Check border color of the email field', async () => {
       await registrationForm.emailField.focus();
       await registrationForm.emailField.blur();
       await expect(registrationForm.emailField).toHaveCSS('color','rgb(73, 80, 87)');
@@ -114,19 +114,19 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Password" field', () => {
 
-    test('Enter incorrect password', async ({ page }) => {
+    test('Enter incorrect password', async () => {
       await registrationForm.passwordField.pressSequentially(incorrectPassword);
       await registrationForm.reenterPasswordField.focus();
       await expect(registrationForm.errorNote).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
     });
 
-    test('Leave password field blank', async ({ page }) => {
+    test('Leave password field blank', async () => {
       await registrationForm.passwordField.focus();
       await registrationForm.passwordField.blur();
       await expect(registrationForm.errorNote).toHaveText('Password required');
     });
 
-    test('Check border color of the password field', async ({ page }) => {
+    test('Check border color of the password field', async () => {
       await registrationForm.passwordField.focus();
       await registrationForm.passwordField.blur();
       await expect(registrationForm.passwordField).toHaveCSS('color','rgb(73, 80, 87)');
@@ -135,21 +135,21 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Re-enter password" field', () => {
 
-    test('Enter non-matching password', async ({ page }) => {
+    test('Enter non-matching password', async () => {
       await registrationForm.passwordField.pressSequentially(correctPassword);
       await registrationForm.reenterPasswordField.pressSequentially('1234Test');
       await registrationForm.emailField.focus();
       await expect(registrationForm.errorNote).toHaveText('Passwords do not match');
     });
 
-    test('Leave re-enter password field blank', async ({ page }) => {
+    test('Leave re-enter password field blank', async () => {
       await registrationForm.passwordField.pressSequentially(correctPassword);
       await registrationForm.reenterPasswordField.focus();
       await registrationForm.reenterPasswordField.blur();
       await expect(registrationForm.errorNote).toHaveText('Re-enter password required');
     });
 
-    test('Check border color of the re-enter password field', async ({ page }) => {
+    test('Check border color of the re-enter password field', async () => {
       await registrationForm.reenterPasswordField.focus();
       await registrationForm.reenterPasswordField.blur();
       await expect(registrationForm.reenterPasswordField).toHaveCSS('color','rgb(73, 80, 87)');
@@ -158,7 +158,7 @@ test.describe('Testing Registration form', () => {
 
   test.describe('Using "Register" button', () => {
 
-    test('Check disabled button', async ({ page }) => {
+    test('Check disabled button', async () => {
       await registrationForm.nameField.pressSequentially(numbersData);
       await registrationForm.lastNameField.pressSequentially(numbersData);
       await registrationForm.emailField.pressSequentially(incorrectEmail);
